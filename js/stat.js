3a7e5094563e660852c8d479a209337da99cf39f
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-11-21 01:03:30 +0300

    ESLint

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Функция рисует Прямоугольник со скруглением краев
 // ctx (object) - canvas
@@ -7,10 +7,10 @@
 // radius (int) - радиус скругления
 // fill, stroke (boolean) - флаги заливки и обводка
 var roundRect = function (ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke === "undefined" ) {
+  if (typeof stroke === 'undefined') {
     stroke = true;
   }
-  if (typeof radius === "undefined") {
+  if (typeof radius === 'undefined') {
     radius = 5;
   }
   ctx.beginPath();
@@ -31,7 +31,7 @@ var roundRect = function (ctx, x, y, width, height, radius, fill, stroke) {
     ctx.fill();
   }
   return;
-}
+};
 
 // Функция отрисовывает голову котэ
 // ctx (object) - canvas
@@ -39,25 +39,25 @@ var roundRect = function (ctx, x, y, width, height, radius, fill, stroke) {
 // width, heigh (object) - ширина и высота головы
 // fill, stroke (boolean) - флаги заливки и обводка
 var nyanCatHead = function (ctx, x, y, width, height, fill, stroke) {
-  if (typeof stroke === "undefined" ) {
+  if (typeof stroke === 'undefined') {
     stroke = true;
   }
-// Основа головы
+  // Основа головы
   ctx.beginPath();
-  ctx.moveTo(x + width/2, y + height/2);
-  ctx.lineTo(x - width/2, y + height/2);
-  ctx.lineTo(x - width/2 - width * 5 / 28, y + height*1/6);
-  ctx.lineTo(x - width/2 - width * 5 / 28, y - height*1/3);
-  ctx.lineTo(x - width/2 - width * 3 / 28, y - height*1/3);
-  ctx.lineTo(x - width/2 - width * 3 / 28, y - height*5/6);
-  ctx.lineTo(x - width/2 - width *  3 / 28 + width * 9 / 20, y - height*2/3);
-  ctx.moveTo(x + width/2, y + height/2);
-  ctx.lineTo(x + width/2 + width * 5 / 28, y + height*1/6);
-  ctx.lineTo(x + width/2 + width * 5 / 28, y - height*1/3);
-  ctx.lineTo(x + width/2 + width * 3 / 28, y - height*1/3);
-  ctx.lineTo(x + width/2 + width * 3 / 28, y - height*5/6);
-  ctx.lineTo(x + width/2 + width *  3 / 28 - width * 9 / 20, y - height*2/3);
-  ctx.lineTo(x - width/2 - width *  3 / 28 + width * 9 / 20, y - height*2/3);
+  ctx.moveTo(x + width / 2, y + height / 2);
+  ctx.lineTo(x - width / 2, y + height / 2);
+  ctx.lineTo(x - width / 2 - width * 5 / 28, y + height * 1 / 6);
+  ctx.lineTo(x - width / 2 - width * 5 / 28, y - height * 1 / 3);
+  ctx.lineTo(x - width / 2 - width * 3 / 28, y - height * 1 / 3);
+  ctx.lineTo(x - width / 2 - width * 3 / 28, y - height * 5 / 6);
+  ctx.lineTo(x - width / 2 - width * 3 / 28 + width * 9 / 20, y - height * 2 / 3);
+  ctx.moveTo(x + width / 2, y + height / 2);
+  ctx.lineTo(x + width / 2 + width * 5 / 28, y + height * 1 / 6);
+  ctx.lineTo(x + width / 2 + width * 5 / 28, y - height * 1 / 3);
+  ctx.lineTo(x + width / 2 + width * 3 / 28, y - height * 1 / 3);
+  ctx.lineTo(x + width / 2 + width * 3 / 28, y - height * 5 / 6);
+  ctx.lineTo(x + width / 2 + width * 3 / 28 - width * 9 / 20, y - height * 2 / 3);
+  ctx.lineTo(x - width / 2 - width * 3 / 28 + width * 9 / 20, y - height * 2 / 3);
   ctx.closePath();
   if (stroke) {
     ctx.stroke();
@@ -66,24 +66,24 @@ var nyanCatHead = function (ctx, x, y, width, height, fill, stroke) {
     ctx.fillStyle = '#999999';
     ctx.fill();
   }
-// Глаза
+  // Глаза
   ctx.shadowColor = 'black';
   ctx.shadowOffsetX = 0;
   ctx.shadowBlur = 0;
   ctx.shadowOffsetY = 0;
   ctx.beginPath();
-  ctx.arc(x - width * 7 / 28, y - height * 1 / 6, ( width + height ) / 22, 0, 2 * Math.PI, false);
+  ctx.arc(x - width * 7 / 28, y - height * 1 / 6, (width + height) / 22, 0, 2 * Math.PI, false);
   ctx.fillStyle = 'black';
   ctx.stroke();
   ctx.fill();
   ctx.closePath();
   ctx.beginPath();
-  ctx.arc(x + width * 7 / 28, y - height * 1 / 6, ( width + height ) / 22, 0, 2 * Math.PI, false);
+  ctx.arc(x + width * 7 / 28, y - height * 1 / 6, (width + height) / 22, 0, 2 * Math.PI, false);
   ctx.fillStyle = 'black';
   ctx.stroke();
   ctx.fill();
   ctx.closePath();
-// Рот
+  // Рот
   ctx.beginPath();
   ctx.moveTo(x - width * 7 / 28, y + height * 1 / 5);
   ctx.lineTo(x - width * 7 / 28, y + height * 2 / 5);
@@ -92,15 +92,15 @@ var nyanCatHead = function (ctx, x, y, width, height, fill, stroke) {
   ctx.moveTo(x, y + height * 1 / 5);
   ctx.lineTo(x, y + height * 2 / 5);
   ctx.stroke();
-// Нос
+  // Нос
   ctx.beginPath();
-  ctx.arc(x, y, ( width + height ) / 44, 0, 2 * Math.PI, false);
+  ctx.arc(x, y, (width + height) / 44, 0, 2 * Math.PI, false);
   ctx.fillStyle = 'black';
   ctx.stroke();
   ctx.fill();
   ctx.closePath();
   return;
-}
+};
 
 // Функция отрисовывает радугу
 // ctx (object) - canvas
@@ -110,14 +110,15 @@ var nyanCatHead = function (ctx, x, y, width, height, fill, stroke) {
 // lineWidth (int) - толщина радужных линий
 var nyanCatRainbow = function (ctx, x, y, width, height, radius, lineWidth) {
   var dy = 0;
-  var dx1 = 0, dx2 = 0;
+  var dx1 = 0;
+  var dx2 = 0;
   var counter = 0;
   var colorMassive = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet'];
 
-  if (typeof radius === "undefined") {
+  if (typeof radius === 'undefined') {
     radius = 5;
   }
-  if (typeof lineWidth === "undefined") {
+  if (typeof lineWidth === 'undefined') {
     lineWidth = 5;
   }
   ctx.lineWidth = lineWidth;
@@ -140,22 +141,22 @@ var nyanCatRainbow = function (ctx, x, y, width, height, radius, lineWidth) {
     dy = dy + lineWidth / 2;
   }
   return;
-}
+};
 
-function textRender(ctx, x, y, text) {
+var textRender = function (ctx, x, y, text) {
   var current = 0;
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   while (text.indexOf('\n', current) !== -1) {
-    ctx.fillText(text.substring(current, text.indexOf('\n', current)), x, y)
+    ctx.fillText(text.substring(current, text.indexOf('\n', current)), x, y);
     current = text.indexOf('\n', current) + 1;
     y = y + 24;
   }
   ctx.fillText(text.substring(current, text.length), x, y);
   return;
-}
+};
 
-window.renderStatistics = function (ctx, name, times = [1, 2]) {
+window.renderStatistics = function (ctx, name, times) {
   var outText = 'Ура вы победили!\nСписок результатов:';
   var maxTime = 0;
   var colorDelta = 0;
@@ -177,13 +178,17 @@ window.renderStatistics = function (ctx, name, times = [1, 2]) {
 
   textRender(ctx, 130, 40, outText);
 
-  maxTime = times.sort(function (a, b) { return b - a;})[0];
-  ctx.strokeStyle = 'black'
+  maxTime = times.sort(function (a, b) {
+    return b - a;
+  })[0];
+  ctx.strokeStyle = 'black';
   ctx.lineWidth = 1;
   for (i = 0; i < times.length; i++) {
     if (name[i] !== 'Вы') {
       colorDelta = Math.random();
-      if (colorDelta === 0) { colorDelta += 0.1; } 
+      if (colorDelta === 0) {
+        colorDelta += 0.1;
+      }
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     } else {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -194,4 +199,4 @@ window.renderStatistics = function (ctx, name, times = [1, 2]) {
     ctx.fillText(Math.round(times[i]), 120 + 40 * i + 50 * i, 255 - 150 * times[i] / maxTime - 15);
     ctx.fillText(name[i], 120 + 40 * i + 50 * i, 270);
   }
-}
+};
